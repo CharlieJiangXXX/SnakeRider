ecbfd49e33f444ebfe58a6942ecd62e0d4e38806
wookho <ryanchiang2004@gmail.com>
2022-11-04 12:00:43 -0400

    copied scenes

---
+++ SRGame/SREndScene.py
@@ -0,0 +1,8 @@
+from PGLib.PGGame import *
+
+
+class SREndScene(PGScene):
+    def __init__(self, game: PGGame):
+        bg = pygame.Surface(game.screen.get_size(), pygame.SRCALPHA)
+        bg.fill((50, 50, 100))
+        super().__init__(game, bg)
--- SRGame/SRGameScene.py
+++ SRGame/SRGameScene.py
@@ -1,1 +1,9 @@
+from PGLib.PGGame import *
+
+
+class SRBGameScene(PGScene):
+    def __init__(self, game: PGGame):
+        bg = pygame.Surface(game.screen.get_size(), pygame.SRCALPHA)
+        bg.fill((50, 50, 100))
+        super().__init__(game, bg)
 print("hello")
