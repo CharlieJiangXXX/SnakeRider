3a7aaaf6b0a24ea7d12ee4a683f66dd05efd5218
CharlieJiangXXX <45895922+CharlieJiangXXX@users.noreply.github.com>
2022-11-11 13:29:39 -0500

    fix eric's bug. literally one line

--- SRGame/SRGameScene.py
+++ SRGame/SRGameScene.py
@@ -7,8 +7,8 @@ class SRGameScene(PGScene):
     def __init__(self, game: PGGame):
         bg = pygame.Surface(game.screen.get_size(), pygame.SRCALPHA)
         bg.fill((50, 50, 100))
-        wb = Whiteboard(self, 10, 10, 50, 50)
         super().__init__(game, bg)
+        wb = Whiteboard(self, 10, 10, 50, 50)
 
 
 class Whiteboard(PGObject):
